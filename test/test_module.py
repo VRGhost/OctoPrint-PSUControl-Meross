addf5747f54296ae44aa0ac8113a8e6d9536d8b0
Ilja Orlovs <vrghost@gmail.com>
2022-01-29 20:52:08 +0000

    fixing test

--- test/test_module.py
+++ test/test_module.py
@@ -3,4 +3,4 @@ import octoprint_psucontrol_meross
 
 def test_import():
     """A simple test to confirm that the module is importable"""
-    assert octoprint_psucontrol_meross.PSUControl_Meross
+    assert octoprint_psucontrol_meross.PSUControlMeross
