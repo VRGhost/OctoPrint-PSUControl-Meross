b0f23b6ac3d7f792944e2b9011e7d9cac6d4eb34
Ilja Orlovs <vrghost@gmail.com>
2022-01-29 20:40:48 +0000

    this should fix the warning

--- test/test_module.py
+++ test/test_module.py
@@ -1,4 +1,6 @@
+import octoprint_psucontrol_meross
+
+
 def test_import():
     """A simple test to confirm that the module is importable"""
-    import octoprint_psucontrol_meross
     assert octoprint_psucontrol_meross.PSUControl_Meross
