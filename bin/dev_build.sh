72e7c72822b736742aad8cda75cac545fac848c9
Ilja Orlovs <vrghost@gmail.com>
2022-01-30 14:43:13 +0000

    dev build works

--- bin/dev_build.sh
+++ bin/dev_build.sh
@@ -4,8 +4,8 @@ set -xe
 THIS_DIR=$(readlink -f $(dirname "${BASH_SOURCE[0]}"))
 PROJ_ROOT="${THIS_DIR}/.."
 
-#bump2version -h
-#bump2version --list
-#exit 1
 cd "${PROJ_ROOT}"
-bump2version --verbose --allow-dirty --no-commit --no-tag dev
+pytest
+rm -f ./dist/*
+bump2version --allow-dirty --no-commit --no-tag dev
+python python -m build --outdir dist/
