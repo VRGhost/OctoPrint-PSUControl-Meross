0711a51f50f162d9023b212668340ab0012cf185
Actions <github+actions@gmail.com>
2022-02-08 22:07:06 +0000

    Bump version: 0.0.7.dev44 → 0.0.7.dev45

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="OctoPrint-PSUControl-Meross",
-    version="0.0.7.dev44",
+    version="0.0.7.dev45",
     description="Adds Meross Smart Plug support to OctoPrint-PSUControl",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
