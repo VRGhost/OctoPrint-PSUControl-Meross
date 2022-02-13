54589a7196fecf08e9b9dd05615a4e1e060dbaa5
Ilja O <vrghost a] gmail.com>
2022-02-13 13:09:16 +0000

    ghetto async mock

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 setuptools.setup(
     name="OctoPrint-PSUControl-Meross",
-    version="0.2.0",
+    version="0.2.0.dev1",
     description="Adds Meross Smart Plug support to OctoPrint-PSUControl",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
