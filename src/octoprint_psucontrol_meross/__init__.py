362419bd8b6c543a724e1d66beba2ada9172f168
Actions <github+actions@gmail.com>
2022-02-13 18:35:40 +0000

    Bump version: 0.9.1.dev1 → 0.10.0

--- src/octoprint_psucontrol_meross/__init__.py
+++ src/octoprint_psucontrol_meross/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.9.1.dev1"
+__VERSION__ = "0.10.0"
 __author__ = "Ilja Orlovs <vrghost@gmail.com>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = (
