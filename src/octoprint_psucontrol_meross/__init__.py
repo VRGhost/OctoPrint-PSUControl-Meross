a64af058063f8c7f1a7387ea45f58cd3c3883d7c
Ilja O <vrghost a] gmail.com>
2022-02-13 15:35:15 +0000

    0.9.1

--- src/octoprint_psucontrol_meross/__init__.py
+++ src/octoprint_psucontrol_meross/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.9.0"
+__VERSION__ = "0.9.1"
 __author__ = "Ilja Orlovs <vrghost@gmail.com>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = (
