224e6c77e27369490d161fb55cefdc9e62325ed6
Ilja Orlovs <vrghost@gmail.com>
2022-12-18 00:11:51 +0000

    Bump version: 0.12.7 → 0.12.8

--- src/octoprint_psucontrol_meross/__init__.py
+++ src/octoprint_psucontrol_meross/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.12.7"
+__VERSION__ = "0.12.8"
 __author__ = "Ilja Orlovs <vrghost@gmail.com>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = (
