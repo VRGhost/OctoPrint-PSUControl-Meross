3c7c543e40a45a52fdd50a170cfb86ccc8a948e7
jeloneal <48490942+jeloneal@users.noreply.github.com>
2024-01-13 21:55:12 +0100

    bump version

--- src/octoprint_psucontrol_meross/__init__.py
+++ src/octoprint_psucontrol_meross/__init__.py
@@ -1,4 +1,4 @@
-__VERSION__ = "0.13.1"
+__VERSION__ = "0.13.3"
 __author__ = "Ilja Orlovs <vrghost@gmail.com>"
 __license__ = "GNU Affero General Public License http://www.gnu.org/licenses/agpl.html"
 __copyright__ = (
